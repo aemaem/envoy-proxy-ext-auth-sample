08e80cde313ea7e8ceed5604083579596f8c5295
Michael Mair <michael.mair@leftshift.one>
2022-10-05 16:17:09 +0200

    use path prefix workaround to make it work

--- auth-service/app/main.py
+++ auth-service/app/main.py
@@ -1,23 +1,24 @@
 import os
 
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from prometheus_fastapi_instrumentator import Instrumentator
 
 path_prefix = os.getenv("PATH_PREFIX", "")
 
 app = FastAPI(openapi_url=f"{path_prefix}/openapi.json")
-
-
-@app.get(f"{path_prefix}/")
-async def authenticate():
-    # todo: authenticate user
-    return {"message": "Successfully authenticated"}
+security = HTTPBasic()
 
 
 @app.get(f"{path_prefix}/authenticate")
-async def authenticate():
-    # todo: authenticate user
-    return {"message": "Successfully authenticated"}
+async def authenticate(credentials: HTTPBasicCredentials = Depends(security)):
+    if credentials.username == "admin" and credentials.password == "admin":
+        return {"message": "Successfully authenticated"}
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Incorrect email or password",
+        headers={"WWW-Authenticate": "Basic"},
+    )
 
 
 @app.get("/health")
